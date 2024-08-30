d737e4dc586d6d0275a60a9e4501c7f4a1c5c9e4
GiovannaV22 <106689391+GiovannaV22@users.noreply.github.com>
2024-08-30 08:21:24 -0300

    Update script.js

--- IAGihVitória/script.js
+++ IAGihVitória/script.js
@@ -48,6 +48,17 @@ let perguntaAtual;
 function mostraPergunta() {
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
 }
+
+function mostraAlternativas() {
+    for(const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.creatElement("button");
+        botaoAlternativas.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+
 
 mostraPergunta();
