a1d2c5aa71881cd30f70d3b1458817f50dd0ab96
GiovannaV22 <106689391+GiovannaV22@users.noreply.github.com>
2024-09-13 09:05:48 -0300

    Update script.js

--- IAGihVitória/script.js
+++ IAGihVitória/script.js
@@ -6,14 +6,14 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Vocẽ já ouviu falar sobre a IA?",
         alternativas: [
             {
-                texto: "Isso é assustador!",
+                texto: "Sim",
                 afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
             },
             {
-                texto: "Isso é maravilhoso!",
+                texto: "Não",
                 afirmacao: "Quis saber como usar IA no seu dia a dia."
             }
         ]
